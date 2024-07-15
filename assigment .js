0035ec9f399c8f7c14e2258ecb93cdb4d9fd9b89
nimrashahid20 <nimrashahid02007@gmail.com>
2024-07-14 19:15:59 -0700

    assigment
    assigment chapter 3 is about variable initialize and declare

---
+++ assigment .js
@@ -0,0 +1,52 @@
+
+
+//1. Declare a variable called age & assign to it your age. Show
+//your age in an alert box.
+
+let age = alert("I am 17 years old")
+console.log(age)
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+let visitor = alert("you have visited this website 15 times")
+console.log(visitor)
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+let birthYear = "2007"
+document.write("my birth year is"+birthYear)
+document.write("<br>"+"Data type of my declared varaiable is number")
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+
+let visitorName = "JohnDoe"
+let productTitile = "T-Shirts"
+let storename = "XYZ Clothing Store"
+document.write("<br>"+visitorName+" ordered  5 "+productTitile+" on "+storename)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
